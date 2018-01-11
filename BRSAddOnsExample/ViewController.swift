a7f8f2d1e97a7964f759798901ff682cd410fcdc
Bindu R S <bindu93rstvm@gmail.com>
2018-01-11 13:59:47 +0530

    license added

--- BRSAddOnsExample/ViewController.swift
+++ BRSAddOnsExample/ViewController.swift
@@ -1,6 +1,26 @@
 //
 //  ViewController.swift
+//  MIT License
 //
+//  Copyright © 2018 bindurs
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import UIKit
 
@@ -35,6 +55,12 @@ class ViewController: UIViewController {
         customView.borderWidth = 5
         customView.cornerRadius = 5
         self.view.addSubview(customView)
+        
+        let btn = UIButton(frame: CGRect(x: 20, y: customView.frame.origin.y+customView.frame.size.height+20, width: UIScreen.main.bounds.size.width-40, height: 45))
+        btn.setTitle("Click Here !!!", for: UIControlState.normal)
+        btn.setTitleColor(UIColor.white, for: UIControlState.normal)
+        btn.addTarget(self, action: #selector(btnPressed(_:)), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(btn)
     }
     
     @IBAction func btnPressed(_ sender: UIButton) {
